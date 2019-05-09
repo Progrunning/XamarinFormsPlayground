2b94a26e8f8e45809a0fc43d9d34b8db22c40b19
Mikolaj Kieres <Mikolaj Kieres>
2019-05-09 22:14:03 +1000

    Adding custom frame renderer for iOS

--- XamarinFormsPlayground/XamarinFormsPlayground.iOS/AppDelegate.cs
+++ XamarinFormsPlayground/XamarinFormsPlayground.iOS/AppDelegate.cs
@@ -1,20 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-using Foundation;
+﻿using Foundation;
 using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using XamarinFormsPlayground.Common;
+
+[assembly: ResolutionGroupName(Constants.Effects.ResolutionGroupName)]
 
 namespace XamarinFormsPlayground.iOS
 {
-    // The UIApplicationDelegate for the application. This class is responsible for launching the 
-    // User Interface of the application, as well as listening (and optionally responding) to 
+    // The UIApplicationDelegate for the application. This class is responsible for launching the
+    // User Interface of the application, as well as listening (and optionally responding) to
     // application events from iOS.
     [Register("AppDelegate")]
-    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    public partial class AppDelegate : FormsApplicationDelegate
     {
         //
-        // This method is invoked when the application has loaded and is ready to run. In this 
+        // This method is invoked when the application has loaded and is ready to run. In this
         // method you should instantiate the window, load the UI into it and then make the window
         // visible.
         //
@@ -22,7 +23,7 @@ namespace XamarinFormsPlayground.iOS
         //
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
-            global::Xamarin.Forms.Forms.Init();
+            Forms.Init();
             LoadApplication(new App());
 
             return base.FinishedLaunching(app, options);
